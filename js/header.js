cb336f8d055d9d40a8a889f355666d9a58d74665
Guilherme <guilhermesantosalves068@gmail.com>
2025-01-30 21:35:10 -0300

    Header finalizado

---
+++ js/header.js
@@ -0,0 +1,21 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 40)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    navlist.classList.toggle('open');
+    if(window.scrollY < 90){
+        header.classList.toggle ("sticky");
+    }
+    
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
